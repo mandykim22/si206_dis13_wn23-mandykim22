3be445bc396674a01092f39e6257f95e3b45fb82
mandykim22 <122408143+mandykim22@users.noreply.github.com>
2023-04-08 11:47:42 -0400

    Add files via upload

--- discussion_13.py
+++ discussion_13.py
@@ -16,6 +16,9 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    cur.execute("DROP TABLE IF EXISTS employees")
+    cur.execute("CREATE TABLE employees (employee_id INTEGER PRIMARY KEY, first_name VARCHAR(25), last_name VARCHAR(25), job_id INTEGER, hire_date VARCHAR(10), salary INTEGER)")
+    conn.commit()
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -27,19 +30,48 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
+    json_data = json.loads(file_data)
+    for j in json_data:
+        cur.execute("INSERT INTO employees (employee_id, first_name, last_name, job_id, hire_date, salary) VALUES (?, ?, ?, ?, ?, ?)", (j["employee_id"], j["first_name"], j["last_name"], j["job_id"], j["hire_date"], j["salary"]))
+    conn.commit()
+
     pass
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute("SELECT hire_date, job_title FROM employees INNER JOIN jobs ON employees.job_id = jobs.job_id ORDER BY hire_date ASC")
+    result = cur.fetchall()[0][1]
+    return result
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute("SELECT first_name, last_name FROM employees INNER JOIN jobs ON employees.job_id = jobs.job_id WHERE employees.salary < jobs.min_salary OR employees.salary > jobs.max_salary")
+    return cur.fetchall()
     pass
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
+    bounds_dict = []
+    bounds_names = []
+    sal_dict = []
+    sal_names = []
+    cur.execute("SELECT job_title, salary FROM jobs INNER JOIN employees ON employees.job_id = jobs.job_id")
+    for item in cur.fetchall():
+        sal_names.append(item[0])
+        sal_dict.append(item[1])
+
+    cur.execute("SELECT job_title, min_salary, max_salary FROM jobs")
+    for item in cur.fetchall():
+        bounds_names.append(item[0])
+        bounds_names.append(item[0])
+        bounds_dict.append(item[1])
+        bounds_dict.append(item[2])
+    
+    plt.scatter(sal_names, sal_dict)
+    plt.scatter(bounds_names, bounds_dict, marker="x", color="red")
+    plt.show()
     pass
 
 class TestDiscussion12(unittest.TestCase):
